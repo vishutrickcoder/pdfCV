20b302fc72a1644b98e0ea2d9e78bb6b36060db4
Vishu <vs1015242@gmail.com>
2023-01-23 21:08:24 +0530

    Not Working

--- pdfCV/urls.py
+++ pdfCV/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pdf import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name="index"),
+    path('<int:id>/',views.resume,name="resume"),
+    path('list/', views.list, name="list"),
 ]
